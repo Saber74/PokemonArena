b48b8c40297dc54ac07a4acc7f712ca6b78028af
Nizar <nizar.rifai7401@gmail.com>
2020-09-02 23:33:42 -0400

    added the actual final files

--- Pokemon/src/Pokemon.java
+++ Pokemon/src/Pokemon.java
@@ -1,16 +1,24 @@
+/*Pokemon Project
+Pokemon class
+Nizar Alrifai
+One of the biggest aspects of the pokemon project, strings are passed from the text file with
+each line constructed into a new pokemon with its own stats and attributes and attacks
+everything to do with pokemons, ranging from healing/recovering to inflecting damage is done here
+other functions within include checking the state of pokemon(disabled/stun/KO) in addition
+to proper handling of all attacks including getting damage and energy losses*/
 import java.util.*;
 public class Pokemon {
-    public ArrayList<Attack> attacklist= new ArrayList<Attack>();
-    private String name;
-    private int hp;
-    private float currhp;
-    private int energy;
-    private String type;
-    private String resistance;
-    private String weakness;
-    private String state;
-    private boolean stun;
-    public Pokemon(String info) {
+    private final ArrayList<Attack> attacklist= new ArrayList<Attack>();
+    private final String name; //name of pokemon
+    private final int hp; // max hit points of a pokemon, these are never affected
+    private float currhp; //hit points of a pokemon that will change
+    private int energy; //current energy of a pokemon up to a max of 50
+    private final String type; //type of pokemon
+    private final String resistance; //types this pokemon is strong against
+    private final String weakness; //types this pokemon is weak against
+    private String state; // checks for disables
+    private boolean stun; //checks for stun
+    public Pokemon(String info) { //using a string construct a pokemon with all of it's required stats
         String [] stats = info.split(",");
         name = stats[0];
         hp = Integer.parseInt(stats[1]);
@@ -29,44 +37,50 @@ public class Pokemon {
         else {
             weakness = stats[4];
         }
-        state="N/A";
+        state="N/A"; //not yet disabled
         int p=6;
+        //allows for continuation of adding of attacks to the attack list for any number of attacks
+        //works by calling the Attack class to make an attack object and store it in an ArrayList
         for(int i=0; i<Integer.parseInt(stats[5]);i+=1){
             attacklist.add(new Attack(stats[p],stats[p+1],stats[p+2],stats[p+3]));
             p+=4;
         }
-        stun=false;
+        stun=false; //not yet stunned
     }
-    public Boolean isAwake() {
+    public Boolean isAwake() {//checks if the pokemon is knocked out
         if (currhp > 0) {
             return true;
         }
         else return false;
     }
-    public void recoverenergy(){
+    public void recoverenergy(){ //end of turn energy recovery
         energy+=10;
         if(energy>50){
             energy=50;
         }
     }
-    public float getDamage(float damage,Pokemon attacker){
-        if(attacker.state.equals("disabled")){
+    private float getDamage(float damage, Pokemon attacker){/*gets the appropriate damage
+    the pokemon should deal by checking if it is disbaled or is dealing against a weakness or resistance
+    the attacker and the supposed damage are paramters and the function is called by using
+    the pokemon reciving the attack (all attributes directly used belong to defender
+    i.e weakness/resistance)*/
+        if(attacker.state.equals("disabled")){ //subtracts 10 dmg if disabled
             damage-=10;
             if(damage<0){
                 damage=0;
             }
         }
-        if(weakness==attacker.type){
+        if(weakness.equals(attacker.type)){ //doubled damage
             damage*=2;
             System.out.println(ConsoleColors.BLUE+"Super effective!");
         }
-        else if(resistance==attacker.type){
+        else if(resistance.equals(attacker.type)){
             damage*=0.5;
             System.out.println(ConsoleColors.BLUE+"not very effective....!");
         }
         return damage;
     }
-    public boolean isNotStunned(){
+    public boolean isNotStunned(){ //checks if the pokemon is stunned and returns a boolean
         boolean bol;
         if(stun){
             bol= false;
@@ -76,53 +90,63 @@ public class Pokemon {
         }
         return bol;
     }
-    public void unstun(){
+    public void unstun(){ //unstuns the pokemon after a turn where he was stunned
         stun=false;
     }
     public void attack(Attack selectedAttack,Pokemon attacker){
-        attacker.energy-=selectedAttack.energycost;
+        /*taking the defender attributes the function takes the selected attack as a parameter
+        as well as the attack pokemon. This method calls the getDamage mehtod to get the appropriate
+        damage as well as subtracts the energy and applies all possible special effects
+         */
+        attacker.energy-=selectedAttack.energycost; //subtracting energy
         System.out.println(attacker.name+" used:"+selectedAttack.name);
-        float damage=selectedAttack.damage;
-        String special=selectedAttack.special;
-        damage=getDamage(damage,attacker);
-        Random random = new Random();
+        float damage=selectedAttack.damage; //getting initial damage
+        String special=selectedAttack.special; //getting the special effect of the attack
+        damage=getDamage(damage,attacker); //getting the damage after modifiers
+        Random random = new Random(); //for the 50% chances
         int num = random.nextInt(2)+1;
-        int success=1;
+        int success=1; //avoiding magic numbers
         int fail=2;
-        if (special.equals("stun")) {
+        if (special.equals("stun")) { //checks for the possibility of stunning
             if(num==success){
                 stun=true;
                 System.out.println(name+" is stunned");
             }
         }
-        else if(special.equals("wild card")){
+        else if(special.equals("wild card")){ //wild card attack makes damage 0 upon failure
             if(num==fail){
                 damage=0;
                 System.out.println("Wild Card failed!");
             }
         }
-        else if(special.equals("wild storm")){
-            while(num==success){
+        else if(special.equals("wild storm")){//as long as the roll suceeded wild storm continues going on for free
+            //the intial damage is taken off at the end of the attack method while all sequel attacks are handled with in the loop
+            while(num==success){ //keeps going till roll fails
                 System.out.println("Wild storm succeeded");
                 currhp-=damage;
-                num = random.nextInt(2)+1;
+                num = random.nextInt(2)+1; //rerolls
             }
         }
-        else if(special.equals("disable")){
+        else if(special.equals("disable")){//disables the target pokemon
             System.out.println("Enemy got disabled!");
             state="disabled";
         }
-        else if(special.equals("recharge")){
+        else if(special.equals("recharge")){ //recharges energy of user
             attacker.energy+=20;
+            if(attacker.energy>50){
+                attacker.energy=50;
+            }
             System.out.println("20 Energy was restored!");
         }
-        currhp-=damage;
-        if(currhp<0){
+        currhp-=damage; //dealing damage
+        if(currhp<0){ //no such thing as an overkill a pokemon can only be KO
+            //anything less than 0 implies a pokemon is dead which is barbaric
             currhp=0;
         }
         System.out.println(damage+" damage"+" was done");
     }
-    public void battlerecover(){
+    public void battlerecover(){//after winning a fight, pokemons are healed, recover energy and
+        //disables are removed
         currhp+=20;
         if(currhp>hp){
             currhp=hp;
@@ -132,6 +156,7 @@ public class Pokemon {
 
     }
     public String toString(){
+        //formating for pokemon selection
         return String.format(ConsoleColors.BLUE+"\n╔═══════════════════════════════╗\n"+
                         ConsoleColors.BLACK+"║ |Name            %-13s║\n" +
                         ConsoleColors.GREEN+"║ |HP              [%-6.2f/%-3d] ║\n"+
@@ -142,6 +167,7 @@ public class Pokemon {
                                             ,name,currhp,hp,type,resistance,weakness);
     }
     public void battlescreen(Pokemon npc){
+        //method to display pokemons in a fight
         System.out.println(ConsoleColors.BLUE+"╔═══════════════════════════════╗    ╔═══════════════════════════════╗\n"+
                 String.format(ConsoleColors.BLACK+"║ |Name            %-13s║    ║ |Name            %-13s║\n" +
                 ConsoleColors.GREEN+"║ |HP              [%-6.2f/%-3d] ║" +ConsoleColors.RED+" VS "+ConsoleColors.GREEN+ "║ |HP              [%-6.2f/%-3d] ║\n"+
@@ -151,6 +177,8 @@ public class Pokemon {
 
     }
     public ArrayList<Attack> enoughEnergy(){
+        //outputs an available attack ArrayList by checking wether the pokemon has enough energy
+        //for said attack
         ArrayList<Attack> validAttacks=new ArrayList<Attack>();
         for(int i=0;i<attacklist.size();i++){
             Attack currAttack=attacklist.get(i);
